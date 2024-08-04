364270a783d1d74be22116c07cdb5a3b1dc71449
vishesh singh <visheshpratapsingh12@gmail.comgit config --global user.name>
2024-08-04 10:20:40 +0530

    main

---
+++ index.js
@@ -0,0 +1,11 @@
+const image = document.querySelector("img")
+const button = document.querySelector("button")
+
+button.addEventListener("click",function(){
+fetch("https://dog.ceo/api/breeds/image/random")
+.then(response => response.json())
+.then((json) =>{
+    console.log(json.message)
+    image.src = json.message
+})
+})
